32303a71c7f841a211dda111c525d8a3b2eef4f7
PoyDev <poydevelop@gmail.com>
2020-05-06 23:13:14 +0700

    First

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        */
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
+            'name' => 'Admin',
+            'email' => 'poydevelop@gmail.com',
+        ]);
+    }
+}
